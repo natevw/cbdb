36ec3561face598e255a6692d2c6235b57a6c76b
Nathan Vander Wilt <nate@calftrail.com>
2020-12-29 16:37:23 -0800

    more progress on hooks

--- hooks.mjs
+++ hooks.mjs
@@ -1,43 +1,39 @@
 import {
-  useRef, useMemo, useState, useEffect
+  useCallback, useMemo, useState, useEffect
 } from "./vdom.mjs";
 
 import { CoreStore } from "./cbdb.mjs";
 
+// WORKAROUND: https://twitter.com/natevw/status/1344063848521760769
+// TODO: if the imported `useMemo` stops providing semantic stability,
+//       replace this with an implementation that does.
+//  see `useMemo` at https://github.com/preactjs/preact/blob/91016b/hooks/src/index.js#L214
+//  and `argsChanged` at https://github.com/preactjs/preact/blob/91016b/hooks/src/index.js#L376  
+const useStable = useMemo;
 
-const UNSET = Symbol('useStable-uninitialized');
-
-
-
-function useStable(fn, deps) {
-  // WORKAROUND: https://twitter.com/natevw/status/1344063848521760769
-  
-  // TODO: 
-  //  see `useMemo` at https://github.com/preactjs/preact/blob/91016b/hooks/src/index.js#L214
-  //  and `argsChanged` at https://github.com/preactjs/preact/blob/91016b/hooks/src/index.js#L376
-  
-  
-  const ref = useRef(UNSET);
-  // TODO: handle `deps`!
-  if (ref.current === UNSET) {
-    ref.current = fn();
-  }
-  return ref.current;
+function useRefresh() {
+  // HT: https://stackoverflow.com/a/53215514/179583
+  const [, setShamValue] = useState();
+  return useCallback(() =>
+    setShamValue(new Object()),
+  []);
 }
 
+const objectIdentity = d => d;
 
-export function useSyncableStore(initialObjs) {
-  // TODO: React threatens not to rely on semantic equivalence here — migrate to `useRef`?
+export function useSyncableStore(initialObjs, key=objectIdentity) {
   const db = useStable(() =>
     new CoreStore(initialObjs),
-  [initialObjs]);
+  []);
   
-  const [ctr,setCtr] = useState(0);
+  // TODO: could/should we do anything if `initialObjs` changes?
+  //       (seems like we would need to know external identifiers…)
   
+  const forceUpdate = useRefresh();
   useEffect(() =>
     db.subscribe(() => {
       console.log("Database changed.");
-      setCtr(ctr+1);
+      forceUpdate();
     }),
   [db]);
   
